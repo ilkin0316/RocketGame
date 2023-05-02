4f57c1691ba9c8c8162c598336189274441cf076
Ilkin <105630796+ilkin0316@users.noreply.github.com>
2023-05-02 12:59:53 +0400

    Değişiklikler eklendi

--- Rocket Game/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
+++ Rocket Game/Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -6,7 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+///Git yoxlamaaaaaaaaaaa
 namespace RocketGame.Controller
 {
     public class PlayerController : MonoBehaviour
