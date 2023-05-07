51f181060b41fcf57a6517d6e698911b84c14243
Ilkin <105630796+ilkin0316@users.noreply.github.com>
2023-05-08 00:46:38 +0400

    Mover Wall

--- Rocket Game/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
+++ Rocket Game/Assets/GameFolders/Scripts/Concretes/Controllers/MoverWallController.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class MoverWallController : WallController
 {
     [SerializeField] Vector3 _direction;
-    [Range(0f,1f)]
     [SerializeField] float _factor;
+    [SerializeField] float _speed = 1f;
 
     Vector3 _startPosition;
+
+    private const float Full_Circle = Mathf.PI * 2f;
+
     private void Awake()
     {
         _startPosition= transform.position;
@@ -17,8 +20,14 @@ public class MoverWallController : WallController
 
     private void Update()
     {
+        float cycle  = Time.time/_speed;
+        float sinWave = Mathf.Sin(cycle * Full_Circle);
+        _factor =Mathf.Abs(sinWave) ;
+
+
+
         Vector3 offset = _direction * _factor;
-        transform.position = offset - _startPosition;
+        transform.position = offset + _startPosition;
     }
 }
 
